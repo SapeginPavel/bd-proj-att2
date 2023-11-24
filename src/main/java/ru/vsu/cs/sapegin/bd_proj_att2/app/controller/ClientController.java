5f4f623b55c04625085d43e68dd83e6e4424eb21
Pavel Sapegin <pavelsapegin18@gmail.com>
2023-11-24 21:43:10 +0300

    intermediate commit #4

--- src/main/java/ru/vsu/cs/sapegin/bd_proj_att2/app/controller/ClientController.java
+++ src/main/java/ru/vsu/cs/sapegin/bd_proj_att2/app/controller/ClientController.java
@@ -1,18 +1,26 @@
 package ru.vsu.cs.sapegin.bd_proj_att2.app.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import ru.vsu.cs.sapegin.bd_proj_att2.api.ClientApi;
 import ru.vsu.cs.sapegin.bd_proj_att2.api.model.ClientDto;
+import ru.vsu.cs.sapegin.bd_proj_att2.app.mapper.ClientMapper;
+import ru.vsu.cs.sapegin.bd_proj_att2.app.service.impl.ClientServiceImpl;
+import ru.vsu.cs.sapegin.bd_proj_att2.item.model.ClientItem;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class ClientController implements ClientApi {
+
+    private final ClientServiceImpl clientService;
 
     @Override
     public ResponseEntity<List<ClientDto>> getAllClients() {
-        return null;
+        List<ClientItem> allClients = clientService.getAllClients();
+        return ResponseEntity.ok(ClientMapper.INSTANCE.mapToDtos(allClients)); //возвращает HTTP-ответ со статусом 200 и телом ответа, содержащим преобразованные объекты Dto
     }
 
     @Override
